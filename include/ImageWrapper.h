e569933ff56092b6529b49a701e8c5ef24963a30
AspirinZJ <zhangmengwei1996@outlook.com>
2022-06-30 17:57:55 -0400

    First commit

---
+++ include/ImageWrapper.h
@@ -0,0 +1,34 @@
+#include <opencv2/opencv.hpp>
+
+class ImageWrapper
+{
+public:
+	ImageWrapper();
+	virtual ~ImageWrapper();
+
+	bool initImageWrapper(unsigned cols, unsigned rows);
+	cv::Mat getImage(cv::Mat image);
+
+private:
+	cv::Mat srcImage; /**< TODO: describe */
+	cv::Mat dstImage; /**< TODO: describe */
+	cv::Mat reSizeImage; /**< TODO: describe */
+
+	unsigned originalImageCols, originalImageRows, unwarpImageCols, unwarpImageRows; /**< TODO: describe */
+	unsigned displayedHeight = 831, displayedWidth = 1591;
+
+	double *trho = nullptr; /**< TODO: describe */
+	double u0, v0; /**< TODO: describe */
+
+	double *theta_acc = nullptr;
+
+
+	/**
+	* @brief table for looking up the pixel indices: srcImage --> dstImage
+	* @details 2D int array
+	* size: [unWarpImageCols][2 x unWarpImagesRows].
+	* *(*(this->indTable + x) + 2 * y) --> xVal;
+	* *(*(this->indTable + x) + 2 * y + 1) --> yVal;
+	*/
+	short **indTable;
+};
